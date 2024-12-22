32261e8f6949f81b41508248fc9da43f8c47e566
PiX <69745008+pixincreate@users.noreply.github.com>
2024-12-22 22:24:01 +0530

    refactor: simplify print

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use zparse::util_functions::read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (file_type, contents) = read(args).unwrap_or_else(|error| {
+    let (_file_type, contents) = read(args).unwrap_or_else(|error| {
         eprintln!("{}", error);
         std::process::exit(1);
     });
 
-    println!("{:?}\n{}", file_type, contents)
+    println!("{}", contents)
 }

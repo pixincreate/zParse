acfe3dbc698abd7a9532c3594540516c0e3aefb2
PiX <69745008+pixincreate@users.noreply.github.com>
2024-12-22 21:22:15 +0530

    refactor: simplify main and move to `util_functions`

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,12 @@
 use std::env;
-use std::process;
-use zparse::{enums::FileType, util_functions};
+use zparse::util_functions::read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (file_type, contents) = read(args);
+    let (file_type, contents) = read(args).unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        std::process::exit(1);
+    });
 
-    println!("{:?}: {}", file_type, contents)
-}
-
-fn read(args: Vec<String>) -> (FileType, String) {
-    if args.len() != 3 {
-        eprintln!("Usage: cargo run -- <json / toml> <filename>");
-        process::exit(1);
-    }
-
-    let file_type = match args[1].to_lowercase().as_str() {
-        "json" => FileType::Json,
-        "toml" => FileType::Toml,
-        _ => {
-            eprintln!("Invalid file type: {}", args[1]);
-            process::exit(1);
-        }
-    };
-    let file_name = &args[2];
-
-    util_functions::validate_file(file_type.clone(), file_name);
-
-    match util_functions::read_file(file_name) {
-        Ok(contents) => (file_type, contents),
-        Err(e) => {
-            eprintln!("Error reading file: {}", e);
-            process::exit(1);
-        }
-    }
+    println!("{:?}\n{}", file_type, contents)
 }
--- src/util_functions.rs
+++ src/util_functions.rs
@@ -1,6 +1,11 @@
-use crate::enums::{FileError, FileType};
-use std::fs;
-use std::io;
+use crate::enums::{ContentError, FileError, FileType, SkillIssue};
+use serde_json::{Error as SerdeJsonError, Value as SerdeJsonValue};
+use std::{
+    error::Error,
+    process, {fs, io},
+};
+use strum::IntoEnumIterator;
+use toml::{de::Error as TomlError, Value as TomlValue};
 
 pub fn read_file(filename: &str) -> Result<String, FileError> {
     fs::read_to_string(filename).map_err(|e| match e.kind() {
@@ -17,16 +22,100 @@ pub fn validate_file(file_type: FileType, filename: &str) {
             match file_type {
                 FileType::Json => {
                     if file_extension != Some("json") {
-                        eprintln!("Invalid file type: {}", filename);
+                        eprintln!(
+                            "Invalid file type: {}\nSupport types: {}",
+                            filename,
+                            FileType::iter()
+                                .map(|x| format!("{:?}", x).to_lowercase())
+                                .collect::<Vec<String>>()
+                                .join(", ")
+                        );
                         std::process::exit(1);
                     }
                 }
                 FileType::Toml => {
                     if file_extension != Some("toml") {
-                        eprintln!("Invalid file type: {}", filename);
+                        eprintln!(
+                            "Invalid file type: {}\nSupport types: {}",
+                            filename,
+                            FileType::iter()
+                                .map(|x| format!("{:?}", x).to_lowercase())
+                                .collect::<Vec<String>>()
+                                .join(", ")
+                        );
                         std::process::exit(1);
                     }
                 }
+            }
+        }
+    }
+}
+
+pub fn read(args: Vec<String>) -> Result<(FileType, String), Box<dyn Error>> {
+    // args will be 3 if the command is correct. example: /targer/debug/zparse json file.json
+    if args.len() != 2 {
+        return Err(Box::new(SkillIssue::WrongCommand));
+    }
+
+    let file_extension = args[1].split('.').last().unwrap_or_default();
+
+    let file_type = match file_extension.to_lowercase().as_str() {
+        "json" => FileType::Json,
+        "toml" => FileType::Toml,
+        _ => {
+            eprintln!(
+                "Invalid file type: {}\nSupported types: {}",
+                file_extension,
+                FileType::iter()
+                    .map(|x| format!("{:?}", x).to_lowercase())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            );
+            process::exit(1);
+        }
+    };
+    let file_name = &args[1];
+
+    validate_file(file_type.clone(), file_name);
+
+    match read_file(file_name) {
+        Ok(contents) => match file_type {
+            FileType::Json => match parse_json(&contents) {
+                Ok(contents) => Ok(pretty_print(file_type, contents.to_string())),
+                Err(error) => Err(Box::new(ContentError::InvalidJson(error))),
+            },
+            FileType::Toml => match parse_toml(&contents) {
+                Ok(contents) => Ok(pretty_print(file_type, contents.to_string())),
+                Err(error) => Err(Box::new(ContentError::InvalidToml(error))),
+            },
+        },
+        Err(error) => Err(Box::new(error)),
+    }
+}
+
+fn parse_json(contents: &str) -> Result<SerdeJsonValue, SerdeJsonError> {
+    serde_json::from_str(contents)
+}
+
+fn parse_toml(contents: &str) -> Result<TomlValue, TomlError> {
+    toml::from_str(contents)
+}
+
+fn pretty_print(file_type: FileType, content: String) -> (FileType, String) {
+    match file_type {
+        FileType::Json => (
+            file_type,
+            serde_json::to_string_pretty(&content).unwrap_or_default(),
+        ),
+        FileType::Toml => {
+            // Parse the content as TOML first
+            if let Ok(toml_value) = content.parse::<TomlValue>() {
+                (
+                    file_type,
+                    toml::to_string_pretty(&toml_value).unwrap_or_default(),
+                )
+            } else {
+                (file_type, content) // Return original content if parsing fails
             }
         }
     }
